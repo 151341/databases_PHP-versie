7ed5326bf851789210412f9825700b9adfbc66d6
151341 <151341@csgaugustinus.nl>
2022-04-19 10:46:38 +0000

    safasdf

--- public/PHPlessen/_oefen.php
+++ public/PHPlessen/_oefen.php
@@ -1,4 +1,4 @@
 <?php
-// met dubbele slashes maak je een commentaarregel
-echo 'Hello <strong>world</strong>!!';
+$data_wh = array('place' => 'groningen','name' => 'stef','brand' => 'samsung');
+echo "weerstation van {$data_wh['place']} is van {$data_wh['name']} gemaakt door {$data_wh['brand']}";
 ?>
