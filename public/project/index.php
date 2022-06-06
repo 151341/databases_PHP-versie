c720df7ae2db1105a76d3828cb45c8fe696b6fb1
Nynke-van-W <152051@csgaugustinus.nl>
2022-06-06 13:33:12 +0000

    .

--- public/project/index.php
+++ public/project/index.php
@@ -30,10 +30,9 @@ require('includes/functions.inc.php');
   </div>
 
   <div class="column mid">
-    <h2>Welkom!</h2>
-    <h3>Onze Nieuwe Producten</h3>
+    <h2>Onze Nieuwste Producten</h2>
     <?php
-      $sql = "SELECT * FROM products WHERE productsId = (SELECT MAX(productsId) FROM products)";
+      $sql = "SELECT * FROM products ORDER BY productsId DESC LIMIT 3";
       $stmt = mysqli_stmt_init($conn);
       if (!mysqli_stmt_prepare($stmt, $sql)) {
         header("location: select.php?error=stmtfailed");
