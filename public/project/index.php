5c00ae08604b55117a92a0da6c997f00753c4f25
Nynke-van-W <152051@csgaugustinus.nl>
2022-06-06 07:48:23 +0000

    new products

--- public/project/index.php
+++ public/project/index.php
@@ -30,14 +30,60 @@ require('includes/functions.inc.php');
   </div>
 
   <div class="column mid">
-    <h3>Welkom!</h3>
-    <p>bekijk ons uitgebreide assortiment</p>
-  
+    <h2>Welkom!</h2>
+    <h3>Onze Nieuwe Producten</h3>
+    <?php
+      $sql = "SELECT * FROM products WHERE productsId = (SELECT MAX(productsId) FROM products)";
+      $stmt = mysqli_stmt_init($conn);
+      if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: select.php?error=stmtfailed");
+        exit();
+      }   
+    ?>
+    <div class="productengalerij">
+            <?php
+            $result = mysqli_query($conn, $sql);
+            $resultCheck = mysqli_num_rows($result);
+            
+            if ($resultCheck > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    ?>
+                  
+                    <div class="producten">
+                        <?php
+                    if ($row['productsImage']!=null) {
+                        $imglink = "productimg/".$row['productsImage'];
+                        ?>
+                        <img src="<?php echo $imglink; ?>" alt="" height="150" width="150">
+                        <?php
+                    }
+                    echo "<h2>". $row['productsName'] . "</h2>";    
+                    echo "<price>$". $row['productsPrice'] . "</price><br>";  
+                    if ($_SESSION['ismanager'] === 1) {
+                        print '<a href="change_product.php?id=' . $row['productsId'] . '">Change product</a><br>';
+                    }
+                    print '<a href="product.php?id=' . $row['productsId'] . '">View product</a>';
+                    if ($_SESSION['userid']!=null) {
+                        ?>
+                        <form method="post" action="includes/shopping_cart.inc.php">
+                            <input type="number" name="productq" placeholder="quantity" value="1" min="1">
+                            <input type="hidden" name="userid" placeholder="userid" value=<?php echo $_SESSION["userid"] ?>>
+                            <input type="hidden" name="productid" placeholder="productid" value=<?php echo $row['productsId'] ?>>
+                            <button class="buttoncard"  type="submit" name="submit">Add to cart</button>
+                        </form>
+                        <?php
+                    }
+                    ?>
+                    </div>
+                    
+                    <?php
+                }
+            }
+            ?>
+        </div>
+    </div>
 
-    <!-- /<img src="<?php echo $imglink; ?>" alt="" height="100" width="100"> -->
-    <!-- hier ga ik nog ff naar kijken -->
-        
-  </div>
+    
 
   <div class="column side">
     <h3>Datum</h3>
